00e7a629c7373ded749d2b38f6512598f57849be
pchurzin <pchurzin@pchurzin.ru>
2019-02-17 13:04:46 +0300

    added test to JAXB implementation

---
+++ src/test/java/ru/javaops/masterjava/xml/MainXmlTest.java
@@ -0,0 +1,21 @@
+package ru.javaops.masterjava.xml;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+import static ru.javaops.masterjava.xml.MainXml.getProjectUsers;
+
+public class MainXmlTest {
+
+    @Test
+    public void testJaxb() {
+        assertArrayEquals(new String[]{"Admin", "Deleted", "Full Name", "PChurzin"},
+                getProjectUsers("topjava").toArray());
+
+        assertArrayEquals(new String[]{"Admin", "PChurzin"},
+                getProjectUsers("masterjava").toArray());
+
+        assertArrayEquals(new String[]{},
+                getProjectUsers("nosuchproject").toArray());
+    }
+}
