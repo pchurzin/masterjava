c2e9968870e39df5127bdbb3897a8b33adb1bbda
Pavel Churzin <pchurzin@pchurzin.ru>
2019-04-06 13:51:18 +0300

    added projects to the DB schema

--- config_templates/sql/initDB.sql
+++ config_templates/sql/initDB.sql
@@ -1,14 +1,28 @@
 DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS user_seq;
-
 DROP TYPE IF EXISTS user_flag;
-DROP TYPE IF EXISTS group_type;
 
 DROP TABLE IF EXISTS cities;
 DROP SEQUENCE IF EXISTS city_seq;
 
 DROP TABLE IF EXISTS "groups";
+DROP SEQUENCE IF EXISTS group_seq;
+DROP TYPE IF EXISTS group_type;
 
+DROP TABLE IF EXISTS projects;
+DROP SEQUENCE IF EXISTS project_seq;
+
+-- projects
+CREATE SEQUENCE project_seq START 300000;
+
+CREATE TABLE projects
+(
+  id INTEGER PRIMARY KEY DEFAULT nextval('project_seq'),
+  name TEXT NOT NULL UNIQUE,
+  description TEXT NOT NULL
+);
+
+-- groups
 CREATE TYPE group_type AS ENUM ('REGISTERING', 'CURRENT', 'FINISHED');
 
 CREATE SEQUENCE group_seq START 200000;
@@ -17,9 +31,11 @@ CREATE TABLE "groups"
 (
   id INTEGER PRIMARY KEY DEFAULT nextval('group_seq'),
   name TEXT NOT NULL UNIQUE,
-  type group_type NOT NULL
+  type group_type NOT NULL,
+  project INTEGER NOT NULL REFERENCES projects (id)
 );
 
+-- cities
 CREATE SEQUENCE city_seq START 100000;
 
 CREATE TABLE cities
@@ -29,6 +45,7 @@ CREATE TABLE cities
   title TEXT NOT NULL
 );
 
+-- users
 CREATE TYPE user_flag AS ENUM ('active', 'deleted', 'superuser');
 
 CREATE SEQUENCE user_seq START 100000;
