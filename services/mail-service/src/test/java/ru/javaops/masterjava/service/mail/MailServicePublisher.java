06a86361893dcf1b1649712c73a972be5944e869
Pavel Churzin <pchurzin@pchurzin.ru>
2019-06-11 09:56:43 +0300

    use shared wsdl from config folder

--- services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServicePublisher.java
+++ services/mail-service/src/test/java/ru/javaops/masterjava/service/mail/MailServicePublisher.java
@@ -1,12 +1,14 @@
 package ru.javaops.masterjava.service.mail;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.io.Resources;
 import ru.javaops.masterjava.persist.DBITestProvider;
 
 import javax.xml.transform.Source;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.ws.Endpoint;
 import java.io.File;
+import java.net.URL;
 import java.util.List;
 
 public class MailServicePublisher {
@@ -15,9 +17,10 @@ public class MailServicePublisher {
         DBITestProvider.initDBI();
 
         Endpoint endpoint = Endpoint.create(new MailServiceImpl());
+        URL resource = Resources.getResource("WEB-INF/wsdl/mailService.wsdl");
         List<Source> metadata = ImmutableList.of(
                 new StreamSource(
-                        new File("/home/cpv/work/configs/masterjava/wsdl/mailService.wsdl")));
+                        new File(resource.getPath())));
 
         endpoint.setMetadata(metadata);
         endpoint.publish("http://localhost:8080/mail/mailService");
