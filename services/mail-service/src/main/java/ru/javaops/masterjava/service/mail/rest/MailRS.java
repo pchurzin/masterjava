9408d3074ebe3261c1ab39dd80fbf6ebce79e9ba
Pavel Churzin <pchurzin@pchurzin.ru>
2019-07-11 18:51:12 +0300

    attachments in REST

--- services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/rest/MailRS.java
+++ services/mail-service/src/main/java/ru/javaops/masterjava/service/mail/rest/MailRS.java
@@ -1,14 +1,22 @@
 package ru.javaops.masterjava.service.mail.rest;
 
 
+import org.glassfish.jersey.media.multipart.FormDataContentDisposition;
+import org.glassfish.jersey.media.multipart.FormDataParam;
 import org.hibernate.validator.constraints.NotBlank;
+import ru.javaops.masterjava.service.mail.Attachment;
 import ru.javaops.masterjava.service.mail.GroupResult;
 import ru.javaops.masterjava.service.mail.MailServiceExecutor;
 import ru.javaops.masterjava.service.mail.MailWSClient;
+import ru.javaops.masterjava.service.mail.util.Attachments;
 import ru.javaops.masterjava.web.WebStateException;
 
-import javax.ws.rs.*;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.io.InputStream;
 import java.util.Collections;
 
 @Path("/")
@@ -23,10 +31,16 @@ public class MailRS {
     @POST
     @Path("send")
     @Produces(MediaType.APPLICATION_JSON)
-    public GroupResult send(@NotBlank @FormParam("users") String users,
-                            @FormParam("subject") String subject,
-                            @NotBlank @FormParam("body") String body) throws WebStateException {
-
-        return MailServiceExecutor.sendBulk(MailWSClient.split(users), subject, body, Collections.emptyList());
+    public GroupResult send(@NotBlank @FormDataParam("users") String users,
+                            @FormDataParam("subject") String subject,
+                            @NotBlank @FormDataParam("body") String body,
+                            @FormDataParam("attach") InputStream file,
+                            @FormDataParam("attach") FormDataContentDisposition disposition) throws WebStateException {
+        if (file == null) {
+            return MailServiceExecutor.sendBulk(MailWSClient.split(users), subject, body, Collections.emptyList());
+        } else {
+            Attachment attachment = Attachments.getAttachment(disposition.getFileName(), file);
+            return MailServiceExecutor.sendBulk(MailWSClient.split(users), subject, body, Collections.singletonList(attachment));
+        }
     }
 }
